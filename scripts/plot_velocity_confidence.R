3eb0b126fc6924803186841fd2ce870e4f72ed75
Charlotte Soneson <charlottesoneson@gmail.com>
2020-10-16 15:43:42 +0200

    Estimate R2

--- scripts/plot_velocity_confidence.R
+++ scripts/plot_velocity_confidence.R
@@ -12,12 +12,14 @@ suppressPackageStartupMessages({
   library(readr)
   library(UpSetR)
   library(pheatmap)
+  library(Seurat)
 })
 
 methods <- strsplit(methods, ",")[[1]]
 names(methods) <- methods
 
 print(topdir)
+print(velosuffix)
 print(plothelperscript)
 print(dataset)
 print(methods)
@@ -28,15 +30,39 @@ source(plothelperscript)
 methods_short <- shorten_methods(methods)
 
 cellinfo <- lapply(methods, function(nm) {
-  readr::read_csv(paste0(topdir, "/plots/velocity/anndata_", nm, "/anndata_", nm, 
+  readr::read_csv(paste0(topdir, "/plots/velocity", velosuffix, 
+                         "/anndata_", nm, "/anndata_", nm, 
                          "_cell_info.csv")) %>%
     dplyr::mutate(method = nm)
 })
 geneinfo <- lapply(methods, function(nm) {
-  readr::read_csv(paste0(topdir, "/plots/velocity/anndata_", nm, "/anndata_", nm, 
+  gi <- readr::read_csv(paste0(topdir, "/plots/velocity", velosuffix, 
+                         "/anndata_", nm, "/anndata_", nm, 
                          "_gene_info.csv")) %>%
     dplyr::mutate(method = nm)
+
+  ## Estimate r2 value for line (through origin) fit to Mu vs Ms
+  w <- ReadH5AD(file.path(topdir, paste0(
+    "output/anndata_with_velocity", velosuffix, "/anndata_", 
+    nm, "_with_velocity.h5ad")))
+  Ms <- as.matrix(GetAssayData(GetAssay(w, "Ms")))
+  Mu <- as.matrix(GetAssayData(GetAssay(w, "Mu")))
+  calc_gamma <- vapply(seq_len(nrow(Ms)), function(i) {
+    sum(Ms[i, ] * Mu[i, ])/sum(Ms[i, ] * Ms[i, ])
+  }, 0)
+  est_r2 <- vapply(seq_len(nrow(Ms)), function(i) {
+    sstotal <- sum((Mu[i, ] - mean(Mu[i, ])) ^ 2)
+    ssregr <- sum((Mu[i, ] - calc_gamma[i] * Ms[i, ]) ^ 2)
+    r2 <- 1 - ssregr/sstotal
+    r2
+  }, 0)
+  
+  gi %>% dplyr::left_join(
+    data.frame(index = rownames(Ms),
+               est_r2 = est_r2),
+    by = "index")
 })
+
 
 velocity_confidence <- do.call(dplyr::bind_rows, lapply(cellinfo, function(w) {
   w %>% dplyr::select(index, method, contains("clusters"), velocity_confidence, 
@@ -54,7 +80,7 @@ if ("clusters" %in% colnames(velocity_confidence) && !is.null(cluster_levels[[da
 velocity_genes <- do.call(dplyr::bind_rows, lapply(geneinfo, function(w) {
   w %>% dplyr::select(index, method, 
                       velocity_score, fit_alpha, fit_beta, fit_gamma, 
-                      fit_likelihood)
+                      fit_likelihood, est_r2, contains("fit_r2"))
 })) %>%
   dplyr::left_join(methods_short, by = "method")
 
@@ -155,6 +181,26 @@ ggplot(velocity_genes, aes(x = method_short, y = fit_likelihood)) +
         axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
   labs(title = paste0(gsub("_", " ", dataset), ", fit likelihood, per gene"),
        subtitle = paste0(""))
+
+if ("fit_r2" %in% colnames(velocity_genes)) {
+  print(ggplot(velocity_genes, aes(x = method_short, y = fit_r2)) + 
+          geom_boxplot(aes(fill = mtype), alpha = 0.25) + 
+          theme_bw() + coord_cartesian(ylim = c(-1, NA)) + 
+          scale_fill_manual(values = base_method_colors, name = "") + 
+          theme(legend.position = "none",
+                axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
+          labs(title = paste0(gsub("_", " ", dataset), ", fit R2, per gene"),
+               subtitle = paste0("")))
+}
+
+print(ggplot(velocity_genes, aes(x = method_short, y = est_r2)) + 
+        geom_boxplot(aes(fill = mtype), alpha = 0.25) + 
+        theme_bw() + coord_cartesian(ylim = c(-1, NA)) + 
+        scale_fill_manual(values = base_method_colors, name = "") + 
+        theme(legend.position = "none",
+              axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
+        labs(title = paste0(gsub("_", " ", dataset), ", est R2, per gene"),
+             subtitle = paste0("")))
 
 dev.off()
 
